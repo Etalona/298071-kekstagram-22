55581f216fd2a35b6563d6060f81b2a743cc185c
emma9_000 <etalona@bk.ru>
2021-02-01 16:17:30 +0700

    Добавлены 2 функции. Вычисление случайного целого числа из выбранного диапазона включительно. Вычисление длины введенного комментария (универсальна).

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+function getRandomIntInclusive(min, max) {
+  if (min < 0) {
+    return 'Диапазон может быть только положительный, включая ноль.';
+  } else if (max <= min) {
+    return 'Недопустимо передавать «до» меньшее, чем значение «от», или равное ему.'
+  } else if (max > min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  }
+}
+
+function checkLengthComment(checkedString, maxLength) {
+  return checkedString.length <= maxLength;
+}
