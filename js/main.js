6ae095cfc4b41fed21328a124569868f3fb1ac6b
emma9_000 <etalona@bk.ru>
2021-02-26 16:09:57 +0700

    Реализован сценарий просмотра фотографий в полноразмерном режиме

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 import {comments, names} from './data.js';
 import {createDescriptions, checkLengthComment} from './util.js';
 import {showUsersPictures} from './picture.js';
+import {pictureClickHandler} from './big-picture.js';
 
 const pictures = createDescriptions(comments, names);
 showUsersPictures(pictures);
 checkLengthComment('sjefhksjfh', 90);
+
+document.querySelector('.pictures').addEventListener('click', function(evt) {
+  pictureClickHandler(evt, pictures);
+});
