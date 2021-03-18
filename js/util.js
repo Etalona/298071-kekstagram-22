28f9b66caa310f3c91b7a12f1c94f66cc88961de
emma9_000 <etalona@bk.ru>
2021-03-18 17:25:37 +0700

    Добавлен фильтр

--- js/util.js
+++ js/util.js
@@ -44,4 +44,17 @@ const closeMessage = function () {
   });
 };
 
-export { showAlert, showInfoUpload};
+const chooseRandom = function(arr, num = 1)  {
+  const res = [];
+  for(let i = 0; i < num; ){
+    const random = Math.floor(Math.random() * arr.length);
+    if(res.indexOf(arr[random]) !== -1){
+      continue;
+    }
+    res.push(arr[random]);
+    i++;
+  }
+  return res;
+};
+
+export { showAlert, showInfoUpload, chooseRandom};
